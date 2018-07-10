902c3d43d906ed943c1111e85a37178c61cfd078
gungun92 <dawn_shadow96@yahoo.co.id>
2018-07-10 19:36:12 +0700

    Update
    hahaha

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -5,18 +5,21 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <head>
         <meta charset="utf-8">
         <title>TravelOra : Travel pakai Pesawat</title>
+        <link rel="stylesheet" href="<?php echo base_url() . "assets/bootstrap/css/bootstrap.min.css"; ?>" media="screen,projection" />
+        <link href="<?php echo base_url() . "assets/fontawesome/web-fonts-with-css/css/fontawesome-all.css"; ?>" rel="stylesheet" />
         <link rel="stylesheet" href="<?php echo base_url()."assets/styleWelcome.css"; ?>" media="screen,projection" />
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <script src="<?php echo base_url() . 'assets/bootstrap/js/bootstrap.min.js'; ?>"></script>
+        
     </head>
     <body>
-        <div class="header">
+        <div class="headers">
             <center><h1><i class="fa fa-cloud" style="font-size: 18px;"></i>TravelOra</h1>
                 <h5>"Mau Travel? Yakinin travel dolo, Pesan Tiket Kemudian"</h5></center>
         </div>
         <div class="nav-bar">
             <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Cek Tiket</a></li>
+                <li><a href="<?php echo base_url() . 'index.php'; ?>">Home</a></li>
+                <li><a href="<?php echo base_url() . 'index.php/tiket'; ?>">Cek Tiket</a></li>
                 <li><a href="#">Cek Pesanan</a></li>
             </ul>
         </div>
